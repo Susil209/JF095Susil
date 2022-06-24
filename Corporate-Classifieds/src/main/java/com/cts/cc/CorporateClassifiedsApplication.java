a07d6cec606a5bb3c37627d2150aa1d9c6cbe9b8
Susil Kumar Nayak <1807075@kiit.ac.in>
2022-06-24 21:33:18 +0530

    first commit

---
+++ Corporate-Classifieds/src/main/java/com/cts/cc/CorporateClassifiedsApplication.java
@@ -0,0 +1,13 @@
+package com.cts.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateClassifiedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateClassifiedsApplication.class, args);
+	}
+
+}
---
+++ Corporate-Classifieds/src/test/java/com/cts/cc/CorporateClassifiedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateClassifiedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
